51399ba19f2c27affe8558a9b65fb7eaff902f5f
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-28 16:02:51 -0700

    add comments to functions

--- dither/corners.py
+++ dither/corners.py
@@ -8,20 +8,38 @@ import os
 data_dir = os.path.dirname(os.path.realpath(__file__))+"/data"
 
 def load_macho_corners():
+	"""
+	Returns the CCD corners of the MACHO camera.
 
+	Returns:
+		corners : *list* of *float*
+			A list of the angular degree offsets of the CCD corners.
+	"""
 	with open('%s/MACHO_corners.dat' % data_dir) as f:
 		corners = eval(''.join(f.readlines()))[0]
 	return corners
 
 def load_decam_corners():
+	"""
+	Returns the CCD corners of the DECam camera.
 
+	Returns:
+		corners : *list* of *float*
+			A list of the angular degree offsets of the CCD corners.
+	"""
 	with open('%s/DECam_corners.dat' % data_dir) as f:
 		corners_dct = eval(''.join(f.readlines()))
 		corners = [v for v in corners_dct.values()]
 	return corners
 
 def load_hsc_corners():
+	"""
+	Returns the CCD corners of the Hyper-Supreme Camera.
 
+	Returns:
+		corners : *list* of *float*
+			A list of the angular degree offsets of the CCD corners.
+	"""
 	with open('%s/HSC_corners.dat' % data_dir) as f:
 		corners_dct = eval(''.join(f.readlines()))
 		corners = [v for v in corners_dct.values()]
