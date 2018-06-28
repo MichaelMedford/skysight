477c67461875a52bce1301fea398bfd266a5e512
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-28 14:37:52 -0700

    fixed loading corners from files

--- dither/corners.py
+++ dither/corners.py
@@ -7,12 +7,6 @@ import os
 
 data_dir = os.path.dirname(os.path.realpath(__file__))+"/data"
 
-def load_hsc_corners():
-
-	with open('%s/HSC_corners.dat' % data_dir) as f:
-		corners = eval(''.join(f.readlines()))[0]
-	return corners
-
 def load_macho_corners():
 
 	with open('%s/MACHO_corners.dat' % data_dir) as f:
@@ -22,5 +16,13 @@ def load_macho_corners():
 def load_decam_corners():
 
 	with open('%s/DECam_corners.dat' % data_dir) as f:
-		corners = eval(''.join(f.readlines()))[0]
+		corners_dct = eval(''.join(f.readlines()))
+		corners = [v for v in corners_dct.values()]
+	return corners
+
+def load_hsc_corners():
+
+	with open('%s/HSC_corners.dat' % data_dir) as f:
+		corners_dct = eval(''.join(f.readlines()))
+		corners = [v for v in corners_dct.values()]
 	return corners
