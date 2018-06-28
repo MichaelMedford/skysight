d87efd0933b291d8ebc1643c37e8d7f04b852fea
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-28 15:24:40 -0700

    remove actions on camera lists

--- dither/dither.py
+++ dither/dither.py
@@ -55,36 +55,4 @@ class Dither:
 		camera.rotate(degrees = self.degrees)
 		camera.translate(ra_offset = self.ra_offset,
 					  dec_offset = self.dec_offset)
-
-def action_
-
-def action_CCDlist(CCDlist, action, name_arr):
-
-	if type(name_arr[0]) in [int,np.int64]:
-		name_arr = [str(n) for n in name_arr]
-
-	for c in CCDlist:
-		if c.name in name_arr:
-			action.apply(c)
-
-def remove_CCDlist(CCDlist, name_arr):
-
-	if type(name_arr[0]) in [int,np.int64]:
-		name_arr = [str(n) for n in name_arr]
-
-	for c in CCDlist:
-		if c.name in name_arr:
-			CCDlist.remove(c)
-
-def copy_CCDlist(CCDlist, name_arr):
-
-	if type(name_arr[0]) in [int,np.int64]:
-		name_arr = [str(n) for n in name_arr]
-
-	new_list = []
-	for name in name_arr:
-		hsc_ccd = [c for c in CCDlist if c.name==name][0].copy()
-		hsc_ccd.name = str(int(CCDlist[-1].name)+1)
-		new_list.append(hsc_ccd.name)
-		CCDlist.append(hsc_ccd)
-	return CCDlist, new_list
+		
