d4d867d65b7fc8e387b528d03e9c5d4648a44954
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-29 14:16:43 -0700

    add author credit

--- skysight/__init__.py
+++ skysight/__init__.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 #
 
+__license__ = "MIT"
 __version__ ="0.2.dev0"
+__author__ = "Michael Medford"
+__email__ = "MichaelMedford@berkeley.edu"
 
 from . import dither
 from . import camera
