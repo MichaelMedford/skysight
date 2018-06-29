1db9b64d064f3d17d2d3a23dc62d2c10a342d0bf
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-29 13:11:38 -0700

    correct corners variables and comments

--- dither/corners.py
+++ dither/corners.py
@@ -7,40 +7,40 @@ import os
 
 data_dir = os.path.dirname(os.path.realpath(__file__))+"/data"
 
-def load_macho_corners():
+def load_machoCorners():
 	"""
 	Returns the CCD corners of the MACHO camera.
 
 	Returns:
-		corners : *list* of *float*
+		machoCorners : *list* of *float*
 			A list of the angular degree offsets of the CCD corners.
 	"""
 	with open('%s/MACHO_corners.dat' % data_dir) as f:
-		corners = eval(''.join(f.readlines()))[0]
-	return corners
+		machoCorners = eval(''.join(f.readlines()))[0]
+	return machoCorners
 
-def load_decam_corners():
+def load_decamCorners():
 	"""
 	Returns the CCD corners of the DECam camera.
 
 	Returns:
-		corners : *list* of *float*
+		decamCorners : *list* of *float*
 			A list of the angular degree offsets of the CCD corners.
 	"""
 	with open('%s/DECam_corners.dat' % data_dir) as f:
 		corners_dct = eval(''.join(f.readlines()))
-		corners = [v for v in corners_dct.values()]
-	return corners
+		decamCorners = [v for v in corners_dct.values()]
+	return decamCorners
 
-def load_hsc_corners():
+def load_hscCorners():
 	"""
 	Returns the CCD corners of the Hyper-Supreme Camera.
 
 	Returns:
-		corners : *list* of *float*
+		hscCorners : *list* of *float*
 			A list of the angular degree offsets of the CCD corners.
 	"""
 	with open('%s/HSC_corners.dat' % data_dir) as f:
 		corners_dct = eval(''.join(f.readlines()))
-		corners = [v for v in corners_dct.values()]
-	return corners
+		hscCorners = [v for v in corners_dct.values()]
+	return hscCorners
