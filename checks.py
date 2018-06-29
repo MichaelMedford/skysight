a97e49afce30e86a55cb78d62f362607a3b3d9d7
Michael Medford <MichaelMedford@berkeley.edu>
2018-06-29 14:39:09 -0700

    move tests folder

--- checks.py
+++ checks.py
@@ -5,6 +5,9 @@ print("System: " + os.name + " " + platform.system() + " " + platform.release())
 import sys
 print("Python: " + sys.version)
 
+import pytest
+print("pytest: " + pytest.__version__)
+
 import numpy
 print("numpy: " + numpy.__version__)
 
